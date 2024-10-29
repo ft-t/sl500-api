4b89946d95dfa11108d2d09930188ca4978bc0bf
Stas Dm <iqpirat@gmail.com>
2024-10-29 16:06:44 +0100

    feat: add workflows

--- sl500_test.go
+++ sl500_test.go
@@ -1,21 +1,28 @@
 package sl500_api_test
 
 import (
-	"testing"
-	"sl500-api"
 	"fmt"
 	"reflect"
+	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ft-t/sl500-api"
 )
 
 func TestCanReadCard(t *testing.T) {
-	reader, err := sl500_api.NewConnection("COM3", sl500_api.Baud.Baud19200, true, 3 * time.Second)
-	reader.RfAntennaSta(sl500_api.AntennaOn)
-	if err != nil {
-		t.Fatal(err)
-	}
-	reader.RfLight(sl500_api.ColorYellow)
-	resp, _ := reader.RfRequest(sl500_api.RequestAll)
+	reader, err := sl500_api.NewConnection("COM3", sl500_api.Baud.Baud19200, true, 3*time.Second)
+	assert.NoError(t, err)
+
+	_, err = reader.RfAntennaSta(sl500_api.AntennaOn)
+	assert.NoError(t, err)
+
+	_, err = reader.RfLight(sl500_api.ColorYellow)
+	assert.NoError(t, err)
+
+	resp, err := reader.RfRequest(sl500_api.RequestAll)
+	assert.NoError(t, err)
 
 	if len(resp) != 2 {
 		t.Fatalf("Wrong length for rf request %v", resp)
@@ -30,23 +37,20 @@ func TestCanReadCard(t *testing.T) {
 		t.Errorf("Capacity length is wrong %v", cardCapacity)
 	}
 
-	reader.RfM1Authentication2(sl500_api.AuthModeKeyA, 0, []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff})
-	blockData, err := reader.RfM1Read(0)
+	_, err = reader.RfM1Authentication2(sl500_api.AuthModeKeyA, 0, []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff})
+	assert.NoError(t, err)
 
-	if err != nil {
-		t.Fatal(err)
-	}
+	blockData, err := reader.RfM1Read(0)
+	assert.NoError(t, err)
 
 	if len(blockData) != 16 {
 		t.Fatalf("Wrong block data length %v", blockData)
 	}
+
 	if !reflect.DeepEqual(blockData[:4], cardId) {
 		t.Fatalf("CardId missmatch")
 	}
-	reader.RfLight(sl500_api.ColorGreen)
 
-	if err != nil {
-		t.Error(err)
-	}
-
+	_, err = reader.RfLight(sl500_api.ColorGreen)
+	assert.NoError(t, err)
 }
